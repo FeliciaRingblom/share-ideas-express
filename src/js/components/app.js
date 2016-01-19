e648b84289945299223c0aec0b41e596721ffd6d
Felicia Ringblom <felicia.ringblom@gmail.com>
2016-01-19 21:16:43 +0100

    project setup

---
+++ src/js/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+require('../../sass/_app.scss');
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Share Ideas</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/js/main.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    },
+    {
+      test: /\.json?$/,
+      loader: 'json'
+    },
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }]
+  }
+};
