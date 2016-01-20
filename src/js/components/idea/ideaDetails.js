41a6a55dae5b703d226371e688b46c0cf1f32661
Felicia Ringblom <felicia.ringblom@gmail.com>
2016-01-20 20:39:17 +0100

    added link to edit mode in details view

--- src/js/components/idea/ideaDetails.js
+++ src/js/components/idea/ideaDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import StoreWatchMixin from '../../mixins/StoreWatchMixin';
 import IdeaActions from '../../actions/ideaActions';
 import IdeaStore from '../../stores/ideaStore';
+import { Link } from 'react-router';
 
 function getIdeaItem( props ) {
   const item = IdeaStore.getIdeas().find( ({ id }) => id === props.params.idea );
@@ -21,6 +22,7 @@ const IdeaItem = (props) => {
         <li>Tech</li>
         <li>App</li>
       </ul>
+      <Link to={`/edit-idea/${props.item.id}`} params={{id: props.item.id}}>Edit</Link>
     </div>
   );
 };
